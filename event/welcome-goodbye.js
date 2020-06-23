09366571a5e6fe3f9822d503dd54d777a7d3c202
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-06-24 00:12:32 +0700

    Update welcome-goodbye.js

--- event/welcome-goodbye.js
+++ event/welcome-goodbye.js
@@ -6,7 +6,7 @@ registerFont(path.join(__dirname, '..', 'cores', 'fonts', 'Heroes Legend.ttf'), 
 module.exports = client => {
     client.on("guildMemberAdd", async (member,msg) => {
 		const channel = member.guild.systemChannel;
-        const firstAvatarURL = member.user.displayAvatarURL({ format: 'png', size: 512 });
+       		const firstAvatarURL = member.user.displayAvatarURL({ format: 'png', size: 512 });
 		try {
 			const firstAvatarData = await request.get(firstAvatarURL);
 			const firstAvatar = await loadImage(firstAvatarData.body);
@@ -22,8 +22,8 @@ module.exports = client => {
 			ctx.fillStyle = 'black';
 			ctx.fillText(member.user.tag, 358, 260);
 			ctx.font = '18px Heroes Legend';
-            ctx.fillStyle = 'white';
-            ctx.fillText(member.guild.name, 408, 342)
+            		ctx.fillStyle = 'white';
+            		ctx.fillText(member.guild.name, 408, 342)
 			return channel.send({ files: [{ attachment: canvas.toBuffer(), name: 'Nancywlcm.png' }] });
 		} catch (err) {
 			return channel.send(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
@@ -32,6 +32,6 @@ module.exports = client => {
 
     client.on("guildMemberRemove", async (member,msg) => {
 		const channel = member.guild.systemChannel;
-        await channel.send(`Nooo, ${member.user.username} Left the server :(`)
+        	await channel.send(`Nooo, ${member.user.username} Left the server :(`)
     });
 }
