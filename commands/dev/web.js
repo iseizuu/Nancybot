87194cdaf7156742e1f2a7b3edf41e8f86d00830
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-06-24 00:15:19 +0700

    Update web.js

--- commands/dev/web.js
+++ commands/dev/web.js
@@ -7,13 +7,12 @@ module.exports = {
     cooldown: 5,
     run: async (client, msg, args) => {
         if(msg.author.id !== '271576733168173057') return
-        const token = "KVSnACmeKsOl7cu-ER354b2cqT4fB830FOOWMYNt6J4Fxmn9UygiWMGZy-_NlTjaNMKM"
-        const webhookClient = new Discord.WebhookClient('719033399058366525', token);
+        const webhookClient = new Discord.WebhookClient('719033399058366525', process.env.WEBTOKEN);
         const channel = client.channels.cache.get('724931004506046496');
-            const webhooks = await channel.fetchWebhooks();
-            const webhook = webhooks.first();
+        const webhooks = await channel.fetchWebhooks();
+        const webhook = webhooks.first();
     
-            await webhook.send(args.join(" "), {
+        await webhook.send(args.join(" "), {
                 username: 'Some Random Guard',
                 avatarURL: 'https://w0.pngwave.com/png/173/214/safety-computer-icons-public-security-safe-png-clip-art.png',
             });
