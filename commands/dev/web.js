d712b3a50c6c16d4d301bbf71768c7fa8b0e552b
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-06-26 10:48:03 +0700

    Up

--- commands/dev/web.js
+++ commands/dev/web.js
@@ -6,14 +6,14 @@ module.exports = {
     description: '',
     cooldown: 5,
     run: async (client, msg, args) => {
-        if(msg.author.id !== '271576733168173057') return
+        if(msg.author.id !== '271576733168173057') return message.channel.send('Whoops');
         const webhookClient = new Discord.WebhookClient('719033399058366525', process.env.WEBTOKEN);
         const channel = client.channels.cache.get('724931004506046496');
         const webhooks = await channel.fetchWebhooks();
         const webhook = webhooks.first();
     
         await webhook.send(args.join(" "), {
-                username: 'Some Random Guard',
+                username: 'Pixelate',
                 avatarURL: 'https://w0.pngwave.com/png/173/214/safety-computer-icons-public-security-safe-png-clip-art.png',
             });
         msg.channel.send('OK')
