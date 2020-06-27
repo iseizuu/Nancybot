912196c530b63a4ca8ab87b74958bf559b11d8bd
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-06-28 06:59:32 +0700

    Emm

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ const client = new Client({
   client.on("ready", async () =>{
     console.log(`${client.user.username} Wake up :)`);
     setInterval(async () => {
-      let ran = [`Nancyy <3`];
+      let ran = [`Nancyy <3`,'WIP 💬'];
       let dom = ran[Math.floor(Math.random() * ran.length)];
       client.user.setPresence({
         activity: {
