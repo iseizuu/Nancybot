c7062d4ab51fb3fd7ac3dc14724131d0ea756920
Vegui Izumi <64743287+VeguiIzumi@users.noreply.github.com>
2020-06-28 16:14:37 +0700

    Create welcome.js

---
+++ commands/Dev/welcome.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require('discord.js');
+const { createCanvas, loadImage, registerFont } = require('canvas');
+const request = require('node-superfetch');
+const path = require('path');
+registerFont(path.join(__dirname, '..', '..', 'cores', 'fonts', 'Heroes Legend.ttf'), { family: 'Heroes Legend' });
+module.exports = {
+    name: 'wc',
+    aliases: ['welcome'],
+    description: '',
+    cooldown: 5,
+    run: async (client, msg, args) => {
+        const gw = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]) || msg.member;
+        const firstAvatarURL = gw.user.displayAvatarURL({ format: 'png', size: 512 });
+		try {
+			const firstAvatarData = await request.get(firstAvatarURL);
+			const firstAvatar = await loadImage(firstAvatarData.body);
+			const base = await loadImage(path.join(__dirname, '..', '..', 'cores', 'img', 'welcome.png'));
+			const canvas = createCanvas(base.width, base.height);
+			const ctx = canvas.getContext('2d');
+			ctx.drawImage(firstAvatar, -6, 35, 400, 400);
+			ctx.drawImage(base, 0, 0);
+			ctx.textAlign = 'left';
+			ctx.textBaseline = 'top';
+			ctx.fillStyle = '#40e9ff';
+			ctx.font = '24px Heroes Legend';
+			ctx.fillStyle = 'black';
+			ctx.fillText(gw.user.tag, 358, 288);
+			ctx.font = '18px Heroes Legend';
+      ctx.fillStyle = 'white';
+       ctx.fillText(msg.guild.name, 408, 358)
+			return msg.channel.send({ files: [{ attachment: canvas.toBuffer(), name: 'Nancywlcm.png' }] });
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+        }
+    }
+}
